8b964e64b1682c0df97bb3387ab45b8965bac8ac
mdasif8393 <md.asif.8393@gmail.com>
2022-09-20 23:18:14 +0600

    add unauthorised review

--- src/pages/Home/Review/Review.js
+++ src/pages/Home/Review/Review.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuth from '../../../hooks/useAuth';
 import { useForm } from "react-hook-form";
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Review.css'
+
 
 const Review = () => {
     const {user} = useAuth();
     const [value, setValue] = useState(5);
     const history = useHistory();
+
+    const [orders, setOrders] = useState([]);
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/orders/${user?.email}`)
+        .then(result => {
+            setOrders(result.data)
+        })
+    },[user.email])
+    
+    const newOrder = orders.find(order=> order.orderStatus === 'shipped');
 
 
     const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
@@ -44,15 +55,18 @@ const Review = () => {
                     />
                 <form onSubmit={handleSubmit(onSubmit)}>
                     {/* <TextField sx={{width: 3/4}} required label="Write a review about us" variant="filled"   rows={4} {...register("review")} /> <br/> */}
-
                     <div className="form-floating">
-                        <textarea required {...register("review")} className="form-control"  style={{height: '150px', width: '300px'}} id="floatingTextarea2"></textarea>
+                        <textarea required {...register("review")} className="form-control"  style={{height: '150px', width: '400px'}} id="floatingTextarea2"></textarea>
                         <label for="floatingTextarea2">Write a review about us</label>
                     </div> <br/>
-
-                    
-
-                    <button className="btn btn-success write-review-button"type="submit"><i class="fas fa-arrow-right"></i> Submit</button>
+                    {
+                        newOrder?.orderStatus !== 'shipped' && 
+                        <div class="alert alert-danger" role="alert">
+                           You can't review because you don't have any shipped order &nbsp;
+                             <Link to="/exploreBikes">Order Bike</Link>
+                      </div>
+                    }
+                    <button className={newOrder?.orderStatus === 'shipped' ? 'btn btn-success write-review-button d-block' : 'd-none'} type="submit"><i class="fas fa-arrow-right"></i> Submit</button>
                 </form>
                 </div>
             </div>
